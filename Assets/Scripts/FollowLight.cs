e6fd1e6aeb0c03df98776f98909ac9f4da6b222e
Alexander Laurencena <76922933+27Salex@users.noreply.github.com>
2026-02-06 18:22:39 +0100

    Update GUI from main (attempt 2) (#8)
    * Art assets implemented
    
    Sprite assets implemented into the project:
    - New sprites loaded in new folders under Sprites
    - Prepared tileset for Factory
    - Imported Cinemachine asset to Unity
    - Added script for handling parallax effect
    - Added background with parallax effect, Cinemachine and the tileset to the SampleScene in order to test them out.
    
    * Animation + light
    
    ---------
    
    Co-authored-by: Brunus <brunomlbw@gmail.com>
    Co-authored-by: Brunus <110927704+BrunoDopico@users.noreply.github.com>
    Co-authored-by: Dualsix <dualsix369@gmail.com>

--- Assets/Scripts/FollowLight.cs
+++ Assets/Scripts/FollowLight.cs
@@ -1,13 +1,32 @@
+using System;
 using UnityEngine;
 
 public class FollowLight : MonoBehaviour
 {
-    [SerializeField] private Transform targetLight;
+    public enum LightType
+    {
+        R,
+        G,
+        B
+    }
+
+    [SerializeField] private LightType lightType;
     [SerializeField] private float maxDistance;
     [SerializeField] private Collider2D collider;
     
     private bool playerWasInRange;
-    
+    private Transform targetLight;
+
+    private void Awake()
+    {
+        if(lightType == LightType.R)
+            targetLight = GameObject.Find("Player").GetComponent<GetLight>().GetRlight;
+        if(lightType == LightType.G)
+            targetLight = GameObject.Find("Player").GetComponent<GetLight>().GetGlight;
+        if(lightType == LightType.B)
+            targetLight = GameObject.Find("Player").GetComponent<GetLight>().GetBlight;
+    }
+
     void FixedUpdate()
     {
         if (Vector3.Distance(transform.parent.position, targetLight.position) <= maxDistance)
