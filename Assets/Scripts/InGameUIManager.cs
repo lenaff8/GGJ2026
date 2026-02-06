e109afce3ef66389afc91a3e530fcc8c37fddc9a
Alexander <alaurencena.edt@gmail.com>
2026-02-06 18:07:36 +0100

    Improve pause menu
    improve pause menu

---
+++ Assets/Scripts/InGameUIManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InGameUIController : MonoBehaviour
+{
+    InGameUIController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
